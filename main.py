327f222591b2f596a89d4e3d88746df1023b06fc
hudson <hudsonpksj@gmail.com>
2024-10-23 19:31:13 -0300

    ambiente configurado

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # python main.py
-print("codigo em producao")
+from sqlalchemy import func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError  # Importa exceções do SQLAlchemy
